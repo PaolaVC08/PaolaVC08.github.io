d3c8d034f822e4dbf126357f15fc70348d1b9439
PaolaVC08 <zS23004729@estudiantes.uv.mx>
2025-03-05 11:43:25 -0600

    Inyeccion de dependencias #2

---
+++ src/app/services/skills-service/skills.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SkillsService {
+  accesoSkills = "skills service running";
+  constructor() { }
+}
